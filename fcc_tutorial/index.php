fb122f173633b819d0dc784192a795e4600b13ed
lone-DO <lotussythe@gmail.com>
2020-04-22 14:58:02 -0400

    staging php enviroment

---
+++ fcc_tutorial/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>
+    PHP Tutorial Website
+  </title>
+</head>
+
+<body>
+  <?php
+  echo "<h1>Hello Globe!</h1>";
+  echo "<hr>";
+  echo "<p>This is a PHP Site</p>";
+  ?>
+  <!-- <h1>Hello World!</h1> -->
+</body>
+
+</html>
