3c41364c298795db9efc9ebebb1caa551d4da808
lone-DO <lotussythe@gmail.com>
2020-04-23 13:21:01 -0400

    String functions

--- fcc_tutorial/index.php
+++ fcc_tutorial/index.php
@@ -10,12 +10,26 @@
 
 <body>
   <?php
-  $string = "To be or not to be";
-  $integer = 30;
-  $float = 3.14;
-  $truthy = true;
-  $falsey = false;
-  $null = null;
+  // $string = "To be or not to be";
+  // $integer = 30;
+  // $float = 3.14;
+  // $truthy = true;
+  // $falsey = false;
+  // $null = null;
+
+  $phrase = "Giraffe Academy";
+
+  echo strtolower($phrase);
+  echo "<hr>";
+  echo strtoupper("dog says 'bark'");
+  echo "<hr>";
+  echo strlen("hello world");
+  echo "<hr>";
+  echo $phrase[0];
+  echo "<hr>";
+  echo str_replace("Giraffe", "Neko", $phrase);
+  echo "<hr>";
+  echo substr($phrase, 8, 3);
 
   echo "<hr>";
   echo "<p>This is a PHP Site</p>";
