6c55ba8d4f9bff2080e62d7fd805451d906758b3
lone-DO <lotussythe@gmail.com>
2020-04-23 13:57:41 -0400

    basic form control

--- fcc_tutorial/index.php
+++ fcc_tutorial/index.php
@@ -9,45 +9,17 @@
 </head>
 
 <body>
-  <?php
-  // $string = "To be or not to be";
-  // $integer = 30;
-  // $float = 3.14;
-  // $truthy = true;
-  // $falsey = false;
-  // $null = null;
+  <form action="index.php" method="get">
+    <label for="name">Name:</label>
+    <input type="text" name="name"><br>
+    <label for="age">Age:</label>
+    <input type="text" name="age"> <br>
+    <input type="submit">
+  </form>
+  <br>
+  <p>Your name is <?php echo $_GET["name"]; ?></p>
+  <p>You are <?php echo $_GET["age"]; ?> Years old</p>
 
-  $modular = 12 % 3;
-  $absolute  = abs(-111);
-  $power = pow(2, 4);
-  $squareroot = sqrt(144);
-  $max = max(2, 10, 50);
-  $min =  min(11, 21, 41);
-  $round = round(3.2);
-  $ceil = ceil(3.1);
-  $floor = floor(3.9);
-
-  echo  $modular;
-  echo "<hr>";
-  echo  $absolute;
-  echo "<hr>";
-  echo  $power;
-  echo "<hr>";
-  echo $squareroot;
-  echo "<hr>";
-  echo $max;
-  echo "<hr>";
-  echo  $min;
-  echo "<hr>";
-  echo  $round;
-  echo "<hr>";
-  echo $ceil;
-  echo "<hr>";
-  echo $floor;
-
-  echo "<hr>";
-  echo "<p>This is a PHP Site $round</p>";
-  ?>
   <!-- <h1>Hello World!</h1> -->
 </body>
 
