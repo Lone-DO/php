300dfff0ed018f2b23e0ecf5e9268593d3c092c9
lone-DO <lotussythe@gmail.com>
2020-04-24 16:11:40 -0400

    If Statments

---
+++ fcc_tutorial/if.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>
+    PHP Tutorial Website
+  </title>
+</head>
+
+<body>
+  <hr>
+
+  <?php
+  $isMale = false;
+  $isTall = true;
+
+  if ($isMale && $isTall) {
+    echo "You are a Tall Male";
+  } elseif ($isMale && !$isTall) {
+    echo "You are a short Male";
+  } elseif (!$isMale && $isTall) {
+    echo "You are a Tall Female";
+  } else {
+    echo "You are a Female";
+  }
+  ?>
+  <hr>
+  <?php
+  function getMax($num1, $num2, $num3)
+  {
+    if ($num1 >= $num2 && $num1 >= $num3) {
+      return $num1;
+    } elseif ($num2 >= $num1 && $num2 >= $num3) {
+      return $num2;
+    } else {
+      return $num3;
+    }
+  }
+
+  echo getMax(122, 20, 2000);
+  ?>
+
+  <hr>
+</body>
+
+</html>
