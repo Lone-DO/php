6299100ed7cdf119bcbad73179e97a8ddce77a14
lone-DO <lotussythe@gmail.com>
2020-04-24 18:03:59 -0400

    object functions/methods

--- fcc_tutorial/classes.php
+++ fcc_tutorial/classes.php
@@ -17,3 +17,26 @@ class Book
 
 $book1 = new Book("Witcher", "Funhaus", 500);
 $book2 = new Book("Lord of the Rings", "JK Rowling", 400);
+
+class Person
+{
+  var $name;
+  var $age;
+  var $gpa;
+  var $gender;
+
+  function __construct($name, $age, $gpa, $gender)
+  {
+    $this->name = $name;
+    $this->age = $age;
+    $this->gender = $gender;
+    $this->gpa = $gpa;
+  }
+  function greet()
+  {
+    echo "Hello, My name is $this->name, I'm a $this->age year old $this->gender, and graduated with a $this->gpa GPA!";
+  }
+}
+
+$person1 = new Person("John Wayne", 54, 3.5, "Male");
+$person1->greet();
